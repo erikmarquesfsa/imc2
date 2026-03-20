05cdbc4d4b77722401534f54d25fe5ba90f63052
erikmarquesfsa <47763378+erikmarquesfsa@users.noreply.github.com>
2026-03-20 19:41:31 -0300

    d

--- js/script.js
+++ js/script.js
@@ -103,7 +103,7 @@ createTable(data);
 [heightInput,weightInput].forEach((el)=>{
     el.addEventListener("input",(e)=>{
         const updateValue = validDigits(e.target.value);
-        e.target.value = updatedValue;
+        e.target.value = updateValue;
     });
 });
 
@@ -124,8 +124,48 @@ calcBtn.addEventListener("click",(e)=>{
         }
     });
 
-    
+    if(!info) return;
+
+    imcNumber.innerText = imc;
+    imcInfo.innerText = info;
+
+    switch(info){
+        case "Magreza":
+            imcNumber.classList.add("low");
+            imcInfo.classList.add("low");
+            break;
+        case "Normal":
+            imcNumber.classList.add("good");
+            imcInfo.classList.add("good");
+            break;
+        case "Sobrepeso":
+            imcNumber.classList.add("low");
+            imcInfo.classList.add("low");
+            break;
+        case "Obesidade":
+            imcNumber.classList.add("medium");
+            imcInfo.classList.add("medium");
+            break;
+        case "Obesidade grave":
+            imcNumber.classList.add("high");
+            imcInfo.classList.add("high");
+            break;
+    }
+
+    showOrHideResults();
 });
+
+clearBtn.addEventListener("click", (e)=>{
+    e.preventDefault();
+    cleanInputs;
+});
+
+backBtn.addEventListener("click",(e)=>{
+    cleanInputs();
+    showOrHideResults();
+});
+
+
 
 
 
